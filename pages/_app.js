ef7202198d99110a037597bac0b48abb06aa7a2b
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-07-29 17:00:38 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
@@ -7,7 +6,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>SKATEZY</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-    
+
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
@@ -16,3 +15,4 @@ const MyApp = ({ Component, pageProps }) => (
 );
 
 export default MyApp;
+
