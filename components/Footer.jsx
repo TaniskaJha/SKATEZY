9e56a8b89c3754bbb22838275732ce5ae6884857
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-07-29 17:11:11 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,6 @@
 
 import { motion } from 'framer-motion';
 import { socials } from '../constants';
-
 import styles from '../styles';
 import { footerVariants } from '../utils/motion';
 
@@ -17,9 +16,12 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-        Enter SKATEZY-WORLD
+          Enter SKATEZY-WORLD
         </h4>
-        <button type="button" className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]">
+        <button
+          type="button"
+          className="flex items-center h-fit py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px]"
+        >
           <img
             src="/headset.svg"
             alt="headset"
