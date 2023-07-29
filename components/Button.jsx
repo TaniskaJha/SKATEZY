92f9dc1f970372836b63599b1321bb9e9482a6ea
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-07-29 17:23:56 +0530

    Update Button.jsx

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,6 +1,5 @@
-'use client';
 import React from 'react';
-import { checkout } from '../checkout.js'; 
+import { checkout } from '../checkout.js';
 
 const Button = ({ children }) => {
   const buttonStyles = {
