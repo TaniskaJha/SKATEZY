2abd838872d42e33f1a3c7e2c8726d6387ba5752
Taniska Jha <112795163+TaniskaJha@users.noreply.github.com>
2023-07-29 17:03:06 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { Footer, Navbar } from '../components';
 import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
 
-
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
@@ -13,13 +12,10 @@ const Home = () => (
     </div>
     <div className="relative">
       <GetStarted />
-  
       <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
     <World />
-
-
     <div className="relative">
       <Insights />
       <div className="gradient-04 z-0" />
